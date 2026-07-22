5a960283289c492e27acd5d06636cbb340346dea
Marco Nunes <marcoaurelion13@gmail.com>
2026-07-21 23:47:36 -0300

    chore: add tests, gitignore, and test scripts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 // Mock getComputedStyle for framer-motion/tests
 Object.defineProperty(window, 'getComputedStyle', { value: () => ({ getPropertyValue: () => '' }) });
